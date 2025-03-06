4f646498a1285399f29718c125ea2411a8c933c4
Dalirs25 <daliaruizsarabia@gmail.com>
2025-03-06 13:05:27 -0600

    Cambios2

---
+++ src/components/Login/ImagenLogin.tsx
@@ -0,0 +1,16 @@
+function ImagenLogin() {
+  return (
+    <div
+      className="h-[500px] flex items-center justify-center text-white text-2xl bg-cover bg-center relative"
+      style={{ backgroundImage: "url('/FondoLogin.png')" }}
+    >
+      <img
+        src="/LogoBlanco.png"
+        alt="Logo"
+        className="absolute top-8 right-4 w-40 h-auto"
+      />
+    </div>
+  );
+}
+
+export default ImagenLogin;
